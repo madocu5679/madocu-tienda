709b346f32090a2177354a7aadd2516178397bab
Manuel <madocu5679@gmail.com>
2022-02-04 10:50:59 +0100

    Primer commit

---
+++ madocu-tienda/src/main/java/com/madocutienda/MadocuTiendaApplication.java
@@ -0,0 +1,13 @@
+package com.madocutienda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MadocuTiendaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MadocuTiendaApplication.class, args);
+	}
+
+}
---
+++ madocu-tienda/src/test/java/com/madocutienda/MadocuTiendaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.madocutienda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MadocuTiendaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
